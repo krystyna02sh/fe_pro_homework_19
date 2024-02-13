8d77a1ea3917ae48d21fce9624da240e05eb67ae
Krystyna Shunkina <christina.shunkina@gmail.com>
2024-02-13 22:27:17 +0200

    hw19 1st try

--- js/index.js
+++ js/index.js
@@ -1,75 +1,74 @@
-const buttonCount = 6;
-const btns = [];
-for (let i = 1; i <= buttonCount; i++) {
-    const btnEl = document.createElement('button');
-    btnEl.textContent = i;
-    document.body.append(btnEl);
-    btns.push(btnEl);
-}
-console.log(btns)
-function makeGame() {
+const btns = document.querySelectorAll('.btn');
+const container = document.getElementById('container')
+function addShadow(el) {
+    el.classList.add("dark");
+};
+const promises = Array.from(btns).map(btn => {
+    const btnNum = parseInt(btn.textContent);
+    if (btnNum % 2 === 0) {
+        return new Promise(resolve => {
+            btn.onclick = () => resolve(btn);
+        });
 
-    function addShadow(el) {
-        el.classList.add("dark");
-    };
+    } else {
+        return btn;
+    }
+});
+promises.forEach(promise => {
+    if (promise instanceof Promise) {
+        promise.then(btn => {
+            addShadow(btn);
+        });
+    }
+});
 
-    // const btnPromises = btns.map(btn => {
-    //     return new Promise(
-    //         resolve => {
-    //             btn.onclick = () => resolve(btn)
-    //         }
-    //     )
-    // }
-    // )
-    // btnPromises.forEach(promise => {
-    //     promise.then(btn => {
-    //         addShadow(btn);
-    //     });
-    // })
+const promises2 = Array.from(btns).map(btn => {
+    const btnNum2 = parseInt(btn.textContent);
+    if (btnNum2 % 2 !== 0) {
+        return new Promise(resolve => {
+            btn.onclick = () => resolve(btn);
+        });
+    } else {
+        return btn;
+    }
+});
+promises2.forEach(promise => {
+    if (promise instanceof Promise) {
+        promise.then(btn => {
+            addShadow(btn);
+        });
+    }
+});
+Promise.all(promises)
+    .then(() => {
+        const textElement = document.createElement('p');
+        textElement.textContent = "Парні кнопки натиснуті";
+        document.body.appendChild(textElement);
+    }
 
-    const btnPromises2 = btns.map(
-        function (button, index) {
-            if (index % 2 === 0) {
-                return new Promise(
-                    resolve => {
-                        button.onclick = () => resolve(button);
-                    }
-                )
-            }
-        }
     )
-    btnPromises2.then(button => {
-        addShadow(button);
+    .catch(error => {
+        console.error("Виникла помилка:", error);
+    });
+Promise.all(promises2)
+    .then(() => {
+        const text = document.createElement('p');
+        text.textContent = "Непарні кнопки натиснуті";
+        document.body.appendChild(text);
+    }
+
+    )
+    .catch(error => {
+        console.error("Виникла помилка:", error);
     });
 
-    Promise.all(btnPromises2)
-        .then(function () {
-            alert("Непарні кнопки натиснуті");
-        })
-    const btnPromises3 = btns.map(
-        function (button, index) {
-            if (index % 2 !== 0) {
-                return new Promise(
-                    resolve => {
-                        button.onclick = () => resolve(button);
-                    }
-                )
-            }
-        }
-    )
-    btnPromises3.forEach(promise => {
-        promise.then(function (button) {
-            addShadow(button);
-        })
+Promise.all([...promises, ...promises2])
+    .then(function () {
+        const textfin = document.createElement('p');
+        textfin.textContent = "Всі кнопки натиснуті";
+        document.body.appendChild(textfin);;
     })
-    Promise.all(btnPromises3)
-        .then(function () {
-            alert("Парні кнопки натиснуті");
-        })
-    Promise.all(btnPromises2, btnPromises3)
-        .then(function () {
-            alert("Всі кнопки натиснуті");
-        })
+    .catch(error => {
+        alert("Виникла помилка:", error);
+    });
 
-}
-makeGame();
